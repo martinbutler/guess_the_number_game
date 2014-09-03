b88206be3bb31d4ca71cab744b3697cd2259d730
Martin Butler <martinebutler@gmail.com>
2014-09-02 23:45:04 -0400

    finalized guessing game with all requested requirements set forth in the assignment.

--- js/application.js
+++ js/application.js
@@ -3,15 +3,25 @@ $(document).ready(function(){
   	var guessesRemaining = 5;
     var theNumber = Math.floor(Math.random() * 100) + 1;
     var difference;  // determine how close the guess is
+    var previousDifference = 0; // 0 will indicate no previous guess
     var guessesMade = {};  
     var currentGuess;
+    var historyMessage = "";
     $('#playAgain').hide();
+    $('#playerGuess').focus();
 
-    $('#checkGuess').click(function(){
+    function gameOverHideAndShow () {
+      $("#checkGuess").hide();
+      $("#giveHint").hide();
+      $('#guessCount').hide();
+      $('#playerGuess').hide();
+      $('#theHistory').hide();
+      $('#playAgain').show();
+    };
+
+    function checkTheGuess() {
       currentGuess = ($('#playerGuess').val());
-      //$('#titleImage').attr('src', './img/chicken.jpg');
       $('#playerGuess').val("");
-      //$('#playerGuess').attr('placeholder', "Input A Number 1-100");
       // check to see if input is a number from 1-100
       if (isNaN(currentGuess) || currentGuess < 1 || currentGuess > 100) {
         $('#result').text("You must enter a number between 1 - 100");
@@ -26,60 +36,87 @@ $(document).ready(function(){
         guessesMade[currentGuess] = guessesRemaining;
       }
 
-    	difference = theNumber - (currentGuess);
-     // $('#result').text(difference); // here for testing
+      difference = theNumber - (currentGuess);
       guessesRemaining--;
 
       if (difference == 0) {
         $('#result').text("Congratulations: You may pass!");
-        $("#checkGuess").hide();
-        $("#giveHint").hide();
-        $('#guessCount').hide();
-        $('#playerGuess').hide();
-        $('#playAgain').show();
+        gameOverHideAndShow();
         $('#titleImage').attr('src', './img/chicken.jpg');
         $('#theLead').text("Winner Winner! Chicken Dinner!");
       } else if (difference <= 5 && difference > 0) {
         $('#result').text("You are very hot. Guess a little higher");
+        historyMessage += currentGuess + " = Very Hot. ";
       } else if (difference >= -5 && difference < 0) {
         $('#result').text("You are very hot. Guess a little lower");
+        historyMessage += currentGuess + " = Very Hot. ";
       } else if (difference <= 10 && difference > 5) {
         $('#result').text("You are hot. Guess higher");
+        historyMessage += currentGuess + " = Hot. ";
       } else if (difference >= -10 && difference < -5) {
         $('#result').text("You are hot. Guess lower");
+        historyMessage += currentGuess + " = Hot. ";
       } else if (difference <= 20 && difference > 10) {
         $('#result').text("You are warm at best. Guess higher");
+        historyMessage += currentGuess + " = Warm. ";
       } else if (difference >= -20 && difference < -10) {
         $('#result').text("You are warm at best. Guess lower");
+        historyMessage += currentGuess + " = Warm. ";
       } else if (difference > 20) {
         $('#result').text("You are cold. Guess much higher");
+        historyMessage += currentGuess + " = Cold. ";
       } else if (difference < 20) {
         $('#result').text("You are cold. Guess much lower");
+        historyMessage += currentGuess + " = Cold. ";
       }
       if (guessesRemaining == 0) {
         $('#result').text("You have been cast into the gorge of eternal peril!");
-        $("#checkGuess").hide();
-        $("#giveHint").hide();
-        $('#guessCount').hide();
-        $('#playerGuess').hide();
-        $('#playAgain').show();
+        $('#titleImage').attr('src', './img/falling.jpg');
+        $('#theLead').hide();
+        gameOverHideAndShow();
       }else{
-        $("#guessCount").text(guessesRemaining + " Guessess Remaining"); 
+        $("#guessCount").text(guessesRemaining + " Guesses Remaining");
+        $('#theHistory').text(historyMessage); 
+        // check to see if player is closer to correct number as compared
+        // to the previous guess.
+        if (previousDifference != 0 && Math.abs(difference) <= Math.abs(previousDifference)){
+          $('#theLead').text("Getting Hotter!");
+        } else if (previousDifference !=0) {
+          $('#theLead').text("Getting Colder!");
+        }
+        previousDifference = difference;
       }
+    };
 
+    $('#checkGuess').click(function(){
+      checkTheGuess();
     });
+
+    $('#playerGuess').keypress(function(event) {
+      if(event.which == 13) {
+        checkTheGuess();
+      }
+    });
+
     $('#playAgain').click(function(){
       $("#checkGuess").show();
       $("#giveHint").show();
       $('#guessCount').show();
       $('#playerGuess').show();
+      $('#theHistory').show();
       $('#playAgain').hide();
+      $('#theLead').show();
       guessesMade = {};
-      $('#result').text("Game as restarted... Good Luck!")
+      historyMessage = "";
+      theNumber = Math.floor(Math.random() * 100) + 1;
       guessesRemaining = 5;
-      $("#guessCount").text(guessesRemaining + " Guessess Remaining");
+      previousDifference = 0;
+      $('#theHistory').text(historyMessage); 
+      $('#result').text("Game has restarted... Good Luck!")
+      $("#guessCount").text(guessesRemaining + " Guesses Remaining");
       $('#theLead').text("Guess correctly to pass or be cast into the gorge of eternal peril!");
       $('#titleImage').attr('src', './img/bridgegorge.jpg');
+      $('#playerGuess').focus();
     }); 
 
     $('#giveHint').click(function() {
